69f292c8ab9f423a4bd9d8b1ec06e72e3e47fafb
wagner.neves <wagner.neves.ext@brf.com>
2022-06-24 14:27:26 -0300

    add di automapper

--- ProjetoUnivespApi/Program.cs
+++ ProjetoUnivespApi/Program.cs
@@ -1,11 +1,29 @@
 using Microsoft.EntityFrameworkCore;
+using ProjetoUnivespApi.Application.Interfaces;
+using ProjetoUnivespApi.Application.Services;
 using ProjetoUnivespApi.Persistence.Context;
+using ProjetoUnivespApi.Persistence.Interfaces;
+using ProjetoUnivespApi.Persistence.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IGeralRepository, GeralRepository>();
+builder.Services.AddScoped<IAlunoRepository, AlunoRepository>();
+builder.Services.AddScoped<IAlunosService, AlunosService>();
+builder.Services.AddScoped<IProfessorRepository, ProfessorRepository>();
+builder.Services.AddScoped<IProfessoresService, ProfessoresService>();
+builder.Services.AddScoped<ILoginRepository, LoginRepository>();
+builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<IPedidosService, PedidosService>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<IProdutosService, ProdutosService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
