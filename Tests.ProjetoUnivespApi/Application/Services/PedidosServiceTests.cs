9a7a8bf5b155376e90b67bd46b924b0a486c35b2
nevesw <wneves@outlook.com>
2022-06-25 11:52:12 -0300

    adicionado metodo de teste pedido service

--- Tests.ProjetoUnivespApi/Application/Services/PedidosServiceTests.cs
+++ Tests.ProjetoUnivespApi/Application/Services/PedidosServiceTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using Moq;
+using ProjetoUnivespApi.Application.Services;
+using ProjetoUnivespApi.Persistence.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +13,27 @@ namespace Tests.ProjetoUnivespApi.Application.Services
 {
     public class PedidosServiceTests
     {
-        [Fact]
-        public void Test1()
+        private readonly PedidosService _pedidoService;
+        private readonly Mock<IPedidoRepository> _pedidoRepoMock = new Mock<IPedidoRepository>();
+        private readonly Mock<IGeralRepository> _geralRepoMock = new Mock<IGeralRepository>();
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+
+        public PedidosServiceTests()
         {
+           // _pedidoService = new PedidosService(_geralRepoMock,_pedidoService,_mapperMock);
 
         }
+
+        [Fact]
+        public async Task AddPedido_DeveAdicionarUmPedido()
+        {
+            // Arrange
+
+
+            // Act
+
+            // Assert
+        }
+
     }
 }
