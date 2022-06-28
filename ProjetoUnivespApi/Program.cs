3e8f6329192d0aa7d9b2100a4fc3e28727b2548b
nevesw <wneves@outlook.com>
2022-06-28 19:05:02 -0300

    config cors

--- ProjetoUnivespApi/Program.cs
+++ ProjetoUnivespApi/Program.cs
@@ -40,6 +40,10 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 
 app.UseHttpsRedirection();
 
